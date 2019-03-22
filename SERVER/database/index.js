6cefd21d09677728e42426f4b517e8648092b4a8
benedicte <benedictemusabimana@gmail.com>
2019-03-22 10:50:25 +0200

    Change for heroku

--- SERVER/database/index.js
+++ SERVER/database/index.js
@@ -1,15 +1,11 @@
-import pg from 'pg';
+import { Pool } from 'pg';
 import dotenv from 'dotenv'
 
 dotenv.config();
-
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
-    
-});
+const connect={
+  connectionString:process.env.DATABASE_URL
+}
+const pool = new Pool(connect);
 
 pool.on('connect',()=>{
   console.log('Connected successfully')
