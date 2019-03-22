409689418e329954499c7cddf1db76fda976b1c4
benedicte <benedictemusabimana@gmail.com>
2019-03-22 02:15:59 +0200

    Chore: test the emailControlles file

--- SERVER/Helper/validation.js
+++ SERVER/Helper/validation.js
@@ -105,43 +105,7 @@ export default class Validate {
 	}
 	
 
-	static validateEmail(email) {
-		const schema = {
-			receiver_username: Joi.string().min(5).max(10).required().error((errors) => {
-				errors.forEach((err) => {
-					switch (err.type) {
-						case 'any.empty':
-							err.message = `Receiver should not be empty!`;
-							break;
-						case 'string.min':
-							err.message = `Receiver should have at least ${err.context.limit} characters!`;
-							break;
-						case 'string.max':
-							err.message = `Receiver should have at most ${err.context.limit} characters!`;
-							break;
-						default:
-							break;
-					}
-				});
-				return errors;
-			}),
-			message: Joi.string().required().error((errors) => {
-				errors.forEach((err) => {
-					switch (err.type) {
-						case `any.empty`:
-							err.message = `Email should not be empty!`;
-							break;
-						default:
-							break;
-					}
-				});
-				return errors;
-			})
-		};
-
-		return Joi.validate(email, schema);
-	}
-
+	
 }
 
 
