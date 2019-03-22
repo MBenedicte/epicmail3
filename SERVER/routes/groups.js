ea013493faa997a645a7f647e2135b72bbd88371
benedicte <benedictemusabimana@gmail.com>
2019-03-22 08:33:49 +0200

    feature: admin add and delete a user from group

--- SERVER/routes/groups.js
+++ SERVER/routes/groups.js
@@ -9,4 +9,5 @@ groupRouter.post('/createGroup',requireAuth,Group.create);
 groupRouter.get('/allGroups',requireAuth,Group.getAllgroups);
 groupRouter.delete('/deletegroup/:id',requireAuth, adminAuth,Group.deleteGroup);
 groupRouter.post('/addnewuser/:id',requireAuth,adminAuth,Group.addMember);
+groupRouter.delete('/delete/:id/users/:id',requireAuth,adminAuth,Group.deleteMember)
 export default groupRouter;
